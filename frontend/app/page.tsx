826ce4ba9641c08d187e0ece5772125637397bd7
binhkid2 <binhkid2@gmail.com>
2026-02-18 22:00:32 +0900

    init Fastify + Nextjs + Postgres

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+ 
+export default function Home() {
+  return (
+   <>
+  <div>
+      <Button>Blank page with nextjs + Shadcn ui</Button>
+    </div>
+   </>
+  );
+}
